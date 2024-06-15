42ffad2b1692d823cd9bdb67f7f42a80221d9fe1
kohasuke <110949581+kobayashi1013@users.noreply.github.com>
2024-06-15 23:30:05 +0900

    プレイヤーの退出

--- Assets/Scripts/Scenes/InGameMulti2/InGameMulti2Manager.cs
+++ Assets/Scripts/Scenes/InGameMulti2/InGameMulti2Manager.cs
@@ -8,9 +8,24 @@ namespace Scenes.InGameMulti2.Manager
 {
     public class InGameMulti2Manager : NetworkBehaviour
     {
+        [Header("Prefabs")]
+        [SerializeField] private GameObject _playerPrefab;
+
         public override void Spawned()
         {
             Debug.Log("Start Multi2");
+
+            //ホスト権限
+            if (NetworkManager.Runner.IsServer)
+            {
+                var playerKeys = new List<PlayerRef>(NetworkManager.Instance.playerList.Keys);
+                foreach (var player in playerKeys)
+                {
+                    //プレイヤーとの紐づけ更新
+                    NetworkObject networkObj = NetworkManager.Runner.Spawn(_playerPrefab, Vector3.zero, Quaternion.identity, player);
+                    NetworkManager.Instance.playerList[player] = networkObj;
+                }
+            }
         }
     }
 }
